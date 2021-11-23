d4f9e9b69b77bdc7514f992bdb54e163877d97e6
cat214 <catou0214@gmail.com>
2021-11-23 12:29:21 +0900

    各要素の配置まで完了

---
+++ js/main.js
@@ -0,0 +1,35 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const el = document.querySelector('.circle');
+
+// トップの見出しスライド
+gsap.from('.slogan-top', {
+    duration: 1,
+    opacity: 0,
+    x: -100,
+})
+gsap.from('.slogan-bottom', {
+    duration: 5,
+    delay: 1,
+    x: -50,
+    opacity: 0
+})
+
+// 理念スライド
+// gsap.from('.philosophy-text-background', {
+//     duration: 5,
+//     x: -1500,
+//     ease: Power3.easeOut,
+//     stagger: 0.1,
+//     scrollTrigger: "philosophy-content-container",
+// });
+
+// gsap.from('.philosophy-text', {
+//     duration: 5,
+//     x: -1500,
+//     ease: Power3.easeOut,
+//     stagger: 0.1,
+//     scrollTrigger: "philosophy-content-container",
+// })
+
+console.log('js finish');
