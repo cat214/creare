fc22dbbb912886077b27d9ebd53a1eeaf121bc41
--local <catou0214@gmail.com>
2021-11-25 18:49:54 +0900

    基本要素を配置完了

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
+console.log('main.js is started');
+
+$(function(){
+  $('.hamburger-button').on('click', function() {
+      console.log('click detected')
+    $(this).toggleClass('active');
+    return false;
+  });
+});
+
+
+
+
+
+
+
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 const el = document.querySelector('.circle');
 
 // トップの見出しスライド
-gsap.from('.slogan-top', {
-    duration: 1,
-    opacity: 0,
-    x: -100,
-})
-gsap.from('.slogan-bottom', {
-    duration: 5,
-    delay: 1,
-    x: -50,
-    opacity: 0
-})
+// gsap.from('.slogan-top', {
+//     duration: 1,
+//     opacity: 0,
+//     x: -100,
+// })
+// gsap.from('.slogan-bottom', {
+//     duration: 5,
+//     delay: 1,
+//     x: -50,
+//     opacity: 0
+// })
 
 // 理念スライド
 // gsap.from('.philosophy-text-background', {
--- js/slide.js
+++ js/slide.js
@@ -1,33 +1,33 @@
-const entrance = document.querySelector('.entrance');
-const interior = document.querySelector('.interior');
-const drawer   = document.querySelector('.drawer');
+// const entrance = document.querySelector('.entrance');
+// const interior = document.querySelector('.interior');
+// const drawer   = document.querySelector('.drawer');
 
-var slide = gsap.timeline();
+// var slide = gsap.timeline();
 
-// 二枚目と三枚目は初期ロードで表示させたくないので最初は透明にしてある
-slide.set(entrance,{zIndex: 3});
-slide.set(interior,{zIndex: 2, opacity: 1});
-slide.set(drawer,  {zIndex: 1, opacity: 1});
+// // 二枚目と三枚目は初期ロードで表示させたくないので最初は透明にしてある
+// slide.set(entrance,{zIndex: 3});
+// slide.set(interior,{zIndex: 2, opacity: 1});
+// slide.set(drawer,  {zIndex: 1, opacity: 1});
 
-slide.to(entrance, {
-    delay: 10,
-    duration: 1,
-    opacity: 0,
-});
-slide.set(entrance,{opacity: 1,zIndex: 0,});
-slide.to(interior, {
-    delay: 10,
-    duration: 1,
-    opacity: 0,
-});
-slide.to(drawer, {
-    delay: 10,
-    duration: 1,
-    opacity: 0,
-});
+// slide.to(entrance, {
+//     delay: 10,
+//     duration: 1,
+//     opacity: 0,
+// });
+// slide.set(entrance,{opacity: 1,zIndex: 0,});
+// slide.to(interior, {
+//     delay: 10,
+//     duration: 1,
+//     opacity: 0,
+// });
+// slide.to(drawer, {
+//     delay: 10,
+//     duration: 1,
+//     opacity: 0,
+// });
 
-slide.set(entrance, {opacity: 1});
-slide.set(interior, {opacity: 1});
-slide.set(drawer,   {opacity: 1});
+// slide.set(entrance, {opacity: 1});
+// slide.set(interior, {opacity: 1});
+// slide.set(drawer,   {opacity: 1});
 
-slide.repeat(-1);
+// slide.repeat(-1);
